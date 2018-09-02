6835fe2f4888dea8080b366bd7d751506775fecf
Nate Wingert <nate.wingert.dev@gmail.com>
2018-09-02 00:03:40 -0600

    Added cli arg for month, more retry wrappers for activity requests

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+/* eslint-disable no-await-in-loop,no-param-reassign,no-restricted-syntax */
 const fetch = require('fetch-retry');
 
 const apiKey = 'd78cb29fa3ab4ff8bacc5be25a96be9f';
 const groupId = '2475694'; // this could change, if so use 'getClanInfo' and dump the new id
 const fetchHeaders = { 'X-API-Key': 'd78cb29fa3ab4ff8bacc5be25a96be9f' };
-const csvPath = './activity_scores.csv';
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+let month = new Date().getMonth();
+if (process.argv.length > 2) {
+  month = Number(process.argv[2]);
+}
+
+let csvPath = './activity_scores.csv';
+if (process.argv.length > 3) {
+  csvPath = process.argv[3];
+}
+
 
 const clanMemberIds = [];
 const activityScores = [];
@@ -63,35 +74,63 @@ async function getActivities(memberId, characterId, startDate, endDate) {
     // if no start or end date is provided then use current month to date
     endDate = new Date();
     startDate = new Date();
-    startDate.setUTCDate(1);
+    startDate.setDate(1);
   }
 
   const activities = [];
-  let activityDate = new Date(endDate.toDateString());
   let page = 0;
   const fetchCount = 100;
   let returnCount = fetchCount;
 
-  while (activityDate > startDate && returnCount === fetchCount) {
-    const response = await fetch(`https://www.bungie.net/Platform/Destiny2/4/Account/${memberId}/Character/${characterId}/Stats/Activities/?count=100&page=${page}&modes='5,7'`, { headers: fetchHeaders });
-    const json = await response.json();
-    const activityList = json.Response.activities;
-    for (const activity of activityList) {
-      activityDate = new Date(activity.period);
-      if (activityDate < startDate) { break; }
-      const activityId = activity.activityDetails.instanceId;
-      const gameResponse = await fetch(`https://www.bungie.net/Platform//Destiny2/Stats/PostGameCarnageReport/${activityId}/`, { headers: fetchHeaders });
-      const gameJson = await gameResponse.json();
-      activities.push(gameJson.Response);
+  let keepGoing = true;
+  while (keepGoing) {
+    // console.log(activityDate);
+    // console.log(startDate);
+
+    let gotActivityJson;
+    let activityList;
+    while (!gotActivityJson) {
+      const gameResponse = await fetch(`https://www.bungie.net/Platform/Destiny2/4/Account/${memberId}/Character/${characterId}/Stats/Activities/?count=100&page=${page}&modes='5,7'`, { headers: fetchHeaders });
+      const contentType = gameResponse.headers.get('content-type');
+      if (contentType && contentType.indexOf('application/json') !== -1) {
+        const json = await gameResponse.json();
+        activityList = json.Response.activities;
+        gotActivityJson = true;
+      } else {
+        console.log('thanks alot bungo');
+      }
     }
-    returnCount = activityList.length;
+    
+    for (const activity of activityList) {
+      const activityDate = new Date(activity.period);
+      if (activityDate >= startDate && activityDate <= endDate) {
+        const activityId = activity.activityDetails.instanceId;
+        let gotJson;
+        while (!gotJson) {
+          const gameResponse = await fetch(`https://www.bungie.net/Platform//Destiny2/Stats/PostGameCarnageReport/${activityId}/`, { headers: fetchHeaders });
+          const contentType = gameResponse.headers.get('content-type');
+          if (contentType && contentType.indexOf('application/json') !== -1) {
+            const gameJson = await gameResponse.json();
+            activities.push(gameJson.Response);
+            gotJson = true;
+          } else {
+            console.log('thanks alot bungo');
+          }
+        }
+      } else if (activityDate < startDate) {
+        keepGoing = false;
+      }
+    }
+    keepGoing = keepGoing && activityList.length === fetchCount;
     page += 1;
   }
   return activities;
 }
 
 async function calculateActivityScore(member) {
-  console.log(`calculating activity score for clan member ${member.name}`);
+  const startDate = new Date(2018, month, 1);
+  const endDate = new Date(2018, month + 1, 0);
+  console.log(`calculating activity score for clan member ${member.name} for ${startDate} - ${endDate}`);
 
   const activityScore = {
     totalClanGames: 0,
@@ -102,7 +141,7 @@ async function calculateActivityScore(member) {
     const characters = await getCharacters(member.id);
     for (const characterId of characters) {
       // console.log(characterId);
-      const activities = await getActivities(member.id, characterId);
+      const activities = await getActivities(member.id, characterId, startDate, endDate);
       for (const activity of activities) {
         for (const entry of activity.entries) {
           const entryMemberId = entry.player.destinyUserInfo.membershipId;
@@ -135,7 +174,7 @@ async function calculateActivityScore(member) {
     clanMemberIds.push(member.id);
   });
 
-  // const clanMembers2 = clanMembers.slice(1, 30);
+  //const subArray = clanMembers.slice(1, 10);
   const splitArray = chunkArray(clanMembers, 20);
   for (let subArray of splitArray) {
     const promises = subArray.map(calculateActivityScore);
