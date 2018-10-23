ac51a27d455e74110c44e29d128ef327ea459b5c
Aaron Ross <superhawk610@gmail.com>
2018-10-23 06:50:14 -0400

    output to csv or json - display game mode names instead of IDs

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const config = require('./lib/config');
-const write = require('./lib/write-to-csv');
+const { writeToCsv, writeToJson } = require('./lib/write');
 const { logErrorAndQuit } = require('./lib/log');
 
 const activity = require('./activity');
@@ -7,11 +7,15 @@ const activity = require('./activity');
 // parse config from CLI / environment
 config.init();
 
+const toCsv = config.get('csv');
+const toJson = config.get('json');
+
 // run report
 async function runReport() {
   try {
     const report = await activity.getActivityReport();
-    write(report);
+    if (toCsv) writeToCsv(report);
+    if (toJson) writeToJson(report);
   } catch (e) {
     logErrorAndQuit(e);
   }
